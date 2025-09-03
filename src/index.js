9a2b36dbf4dc2e66dc9e59f798eae180d8b2f6cc
renan rodrigues dos santos <renandosantos.ti@gmail.com>
2025-09-03 15:02:13 -0300

    montando o esqueleto

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json({message: "testando o get"})
+})
+
+
+app.listen(3000, ()=> {
+    console.log(`Servidor rodando na porta ${port}`)
+})
